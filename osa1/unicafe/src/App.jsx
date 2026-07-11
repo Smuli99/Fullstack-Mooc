d289d794d385a6e94ecb65f20701fc699ba9ec48
Smuli99 <hytonensamu99@gmail.com>
2026-07-11 15:16:51 +0300

    task 1.11 done

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -18,9 +18,10 @@ const Button = ({onClick, text}) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      <p>{text} {value}</p>
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
@@ -34,14 +35,16 @@ const Statistics = ({ good, neutral, bad, total, average, positive }) => {
   }
 
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="total" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="total" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
   );
 };
 
