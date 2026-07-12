2674f43510cd1fa152161bec8fea8ec0b914e726
Smuli99 <hytonensamu99@gmail.com>
2026-07-12 17:43:47 +0300

    task 2.1 done

--- osa1/kurssitiedot/eslint.config.js
+++ osa1/kurssitiedot/eslint.config.js
@@ -21,4 +21,4 @@ export default defineConfig([
       semi: ['error', 'always'],
     },
   },
-])
+]);
--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,40 +1,9 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.part.name} {props.part.exercises}</p>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-  );
-};
+import Course from './components/Course';
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
@@ -47,19 +16,13 @@ const App = () => {
       {
         name: 'State of a component',
         exercises: 14
-      }
+      },
     ]
   };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content 
-        parts={course.parts}
-      />
-      <Total
-        parts={course.parts}
-      />
+      <Course course={course} />
     </div>
   );
 };
