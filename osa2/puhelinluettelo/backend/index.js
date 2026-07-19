0a8ed64e42f3eee9eac74d8c1d70699a668205a6
Smuli99 <hytonensamu99@gmail.com>
2026-07-19 16:58:31 +0300

    task 3.3 done

--- osa2/puhelinluettelo/backend/index.js
+++ osa2/puhelinluettelo/backend/index.js
@@ -43,6 +43,16 @@ app.get('/info', (request, response) => {
   response.send(info);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find(p => p.id === id);
+
+  if (!person) response.status(404).json({
+    error: 'person not found'
+  });
+  else response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
