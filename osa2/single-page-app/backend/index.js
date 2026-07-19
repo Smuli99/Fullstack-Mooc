89960e4deb0cb31039890a3f79a0d7fb1b501dbe
Smuli99 <hytonensamu99@gmail.com>
2026-07-19 18:00:25 +0300

    single-page app updated

--- osa2/single-page-app/backend/index.js
+++ osa2/single-page-app/backend/index.js
@@ -3,6 +3,16 @@ const app = express();
 
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:', request.path);
+  console.log('Body:', request.body);
+  console.log('---');
+  next();
+};
+
+app.use(requestLogger);
+
 let notes = [
   {
     id: "1",
@@ -43,18 +53,38 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end();
 });
 
-app.post('/api/notes', (request, response) => {
+const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => Number(n.id)))
     : 0;
-  
-  const note = request.body;
-  note.id = String(maxId + 1);
+  return String(maxId + 1);
+};
 
+app.post('/api/notes', (request, response) => {
+  const bpdy = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+  
   notes = notes.concat(note);
   
   response.json(note);
-})
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
