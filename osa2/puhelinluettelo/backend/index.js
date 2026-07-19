d1f266832930c7359a92e0b2b98f737417afb91c
Smuli99 <hytonensamu99@gmail.com>
2026-07-19 17:19:36 +0300

    task 3.5 done

--- osa2/puhelinluettelo/backend/index.js
+++ osa2/puhelinluettelo/backend/index.js
@@ -53,10 +53,33 @@ app.get('/api/persons/:id', (request, response) => {
   else response.json(person);
 });
 
+const generateId = () => {
+  const id = Math.floor(Math.random() * 1000000);
+  return String(id);
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) return response.status(400).json({
+    error: 'name missing'
+  });
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number || '',
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id;
   persons = persons.filter(p => p.id !== id);
-  
+
   response.status(204).end();
 });
 
