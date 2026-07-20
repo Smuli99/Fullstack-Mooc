e623b4d0e68b75b6d2c361af42ec9695059f500c
Smuli99 <hytonensamu99@gmail.com>
2026-07-20 15:37:01 +0300

    single-page app updated

--- osa2/single-page-app/backend/index.js
+++ osa2/single-page-app/backend/index.js
@@ -4,9 +4,6 @@ const app = express();
 app.use(express.json());
 app.use(express.static('dist'));
 
-const cors = require('cors');
-app.use(cors());
-
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
   console.log('Path:', request.path);
