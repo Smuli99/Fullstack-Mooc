359b2925e43f3e67aa269c4ddac04b2b13b7badf
Smuli99 <hytonensamu99@gmail.com>
2026-07-20 17:44:12 +0300

    task 3.9 done

--- osa2/puhelinluettelo/backend/index.js
+++ osa2/puhelinluettelo/backend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'));
 
 morgan.token('body', (request => {
   return JSON.stringify(request.body);
@@ -105,7 +106,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
