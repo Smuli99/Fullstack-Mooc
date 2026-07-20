24fba732787f8801105ba5ab57dea89e467baecb
Smuli99 <hytonensamu99@gmail.com>
2026-07-20 13:09:15 +0300

    single-page app updated

--- osa2/single-page-app/backend/index.js
+++ osa2/single-page-app/backend/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'));
+
+const cors = require('cors');
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
@@ -86,7 +90,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
