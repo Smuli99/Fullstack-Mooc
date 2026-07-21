0976870da86f675295d283a2a9e8a6dc92da6447
Smuli99 <hytonensamu99@gmail.com>
2026-07-21 10:25:59 +0300

    uibuild

--- osa2/single-page-app/backend/index.js
+++ osa2/single-page-app/backend/index.js
@@ -36,14 +36,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    });
-  }
 
   const note = new Note({
     content: body.content,
@@ -52,7 +46,8 @@ app.post('/api/notes', (request, response) => {
   
   note.save().then(savedNote => {
     response.json(savedNote);
-  });
+  })
+  .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
