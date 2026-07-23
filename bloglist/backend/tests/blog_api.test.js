225bb73404275686339eea4fe9d7481ca62eb2a0
Smuli99 <hytonensamu99@gmail.com>
2026-07-23 18:33:04 +0300

    task 4.8 done

---
+++ bloglist/backend/tests/blog_api.test.js
@@ -0,0 +1,27 @@
+const { test, describe, beforeEach, after } = require('node:test');
+const assert = require('node:assert');
+const supertest = require('supertest');
+const mongoose = require('mongoose');
+const Blog = require('../models/blog');
+const app = require('../app');
+const helper = require('./test_helper');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
+
+describe('HTML Protocol test', () => {
+  test.only('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
