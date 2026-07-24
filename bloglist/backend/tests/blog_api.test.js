ef3b81fab636a0843536e4ba411676ebc5c35c2f
Smuli99 <hytonensamu99@gmail.com>
2026-07-24 16:57:59 +0300

    task 4.9 done

--- bloglist/backend/tests/blog_api.test.js
+++ bloglist/backend/tests/blog_api.test.js
@@ -14,11 +14,19 @@ beforeEach(async () => {
 });
 
 describe('HTML Protocol test', () => {
-  test.only('blogs are returned as json', async () => {
+  test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/);
+  });
+
+  test('returned blogs use id instead of _id', async () => {
+    const response = await api.get('/api/blogs');
+    response.body.forEach(blog => {
+      assert(blog.id);
+      assert(!blog._id);
+    });
   });
 });
 
