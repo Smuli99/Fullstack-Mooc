3ec0e0f3b98997ce7978d5542ef09b0526b51257
Smuli99 <hytonensamu99@gmail.com>
2026-08-02 23:52:46 +0300

    task 5.7 done

--- bloglist/frontend/src/components/Blog.jsx
+++ bloglist/frontend/src/components/Blog.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+
 const Blog = ({ blog }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const toggleInfo = () => setShowInfo(!showInfo);
+  const buttonText = showInfo
+    ? 'hide'
+    : 'view';
+
   return (
     <div>
-      {blog.title} {blog.author}
+      <div className='blog'>
+        <p>
+          {blog.title} by {blog.author}
+          <button onClick={toggleInfo}>{buttonText}</button>
+        </p>
+
+        {showInfo && (
+          <>
+            <p>{blog.url}</p>
+            <p>
+              likes: {blog.likes}
+              <button>like</button>
+            </p>
+          </>
+        )}
+      </div>
     </div>
   );
 };
