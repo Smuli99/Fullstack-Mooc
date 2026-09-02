a342f6c51cebe599292f4633e5f0a6feae621fba
Smuli99 <hytonensamu99@gmail.com>
2026-09-02 16:12:26 +0300

    task 5.18 done

--- bloglist/e2e-tests/tests/blog_app.spec.js
+++ bloglist/e2e-tests/tests/blog_app.spec.js
@@ -1,5 +1,5 @@
 const { test, describe, beforeEach, expect } = require('@playwright/test');
-const { initializeDB } = require('./helper');
+const { initializeDB, loginWith } = require('./helper');
 
 describe('Blog App', () => {
   beforeEach(async ({ page, request }) => {
@@ -12,5 +12,17 @@ describe('Blog App', () => {
     await expect(page.getByLabel('username')).toBeVisible();
     await expect(page.getByLabel('password')).toBeVisible();
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible();
+  });
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'FooBar', 'salainen123');
+      await expect(page.getByText('John Doe logged in')).toBeVisible();
+    });
+  
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'FooBar', 'sekret');
+      await expect(page.getByText('wrong username or password')).toBeVisible();
+    });
   });
 });
--- bloglist/e2e-tests/tests/helper.js
+++ bloglist/e2e-tests/tests/helper.js
@@ -1,14 +1,21 @@
 const initializeDB = async (req) => {
-  await req.post('/api/testing/reset');
-  await req.post('/api/users', {
+  const response1 = await req.post('/api/testing/reset');
+  const response2 = await req.post('/api/users', {
     data: {
       name: 'John Doe',
       username: 'FooBar',
-      password: 'sekret',
+      password: 'salainen123',
     }
   });
 };
 
+const loginWith = async (page, username, password) => {
+  await page.getByLabel('username').fill(username);
+  await page.getByLabel('password').fill(password);
+  await page.getByRole('button', { name: 'login' }).click();
+};
+
 export {
   initializeDB,
+  loginWith,
 }
