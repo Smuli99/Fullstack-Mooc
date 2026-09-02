7fd38c9338fe49f22581a908ce79527fc10b9a20
Smuli99 <hytonensamu99@gmail.com>
2026-09-02 16:39:06 +0300

    task 5.19 done

--- bloglist/e2e-tests/tests/blog_app.spec.js
+++ bloglist/e2e-tests/tests/blog_app.spec.js
@@ -1,5 +1,5 @@
 const { test, describe, beforeEach, expect } = require('@playwright/test');
-const { initializeDB, loginWith } = require('./helper');
+const { initializeDB, loginWith, createBlog } = require('./helper');
 
 describe('Blog App', () => {
   beforeEach(async ({ page, request }) => {
@@ -23,6 +23,34 @@ describe('Blog App', () => {
     test('fails with wrong credentials', async ({ page }) => {
       await loginWith(page, 'FooBar', 'sekret');
       await expect(page.getByText('wrong username or password')).toBeVisible();
+    });
+
+    describe('When logged in', () => {
+      beforeEach(async ({ page }) => {
+        await loginWith(page, 'FooBar', 'salainen123');
+      });
+
+      test('user can logout', async ({ page }) => {
+        await page.getByRole('button', { name: 'logout' }).click();
+        await expect(page.getByText('Login to Blog App')).toBeVisible();
+        await expect(page.getByText('John Doe logged in')).not.toBeVisible();
+      });
+
+      test('a new blog can be created', async ({ page }) => {
+        const title = 'The Odyssey';
+        const author = 'Cristofer Nolan';
+        const url = 'https://example.com';
+        
+        await createBlog(
+          page, 
+          title,
+          author,
+          url
+        );
+
+        await expect(page.getByText(`\`${title}\` by ${author} added!`)).toBeVisible();
+        await expect(page.getByText(`${title} by ${author}`)).toBeVisible();
+      });
     });
   });
 });
--- bloglist/e2e-tests/tests/helper.js
+++ bloglist/e2e-tests/tests/helper.js
@@ -15,7 +15,18 @@ const loginWith = async (page, username, password) => {
   await page.getByRole('button', { name: 'login' }).click();
 };
 
+const createBlog = async (page, title, author, url) => {
+  await page.getByRole('button', { name: 'new blog' }).click();
+  await page.getByLabel('title').fill(title);
+  await page.getByLabel('author').fill(author);
+  await page.getByLabel('url').fill(url);
+  await page.getByRole('button', { name: 'create' }).click();
+
+  // await page.getByText(`${title} by ${author}`).waitFor();
+};
+
 export {
   initializeDB,
   loginWith,
+  createBlog,
 }
