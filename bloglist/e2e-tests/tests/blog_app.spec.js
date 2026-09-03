2e89293fac6217b1fc9d18c10dfab3b460393ee3
Smuli99 <hytonensamu99@gmail.com>
2026-09-04 00:30:45 +0300

    task 5.20 done

--- bloglist/e2e-tests/tests/blog_app.spec.js
+++ bloglist/e2e-tests/tests/blog_app.spec.js
@@ -1,5 +1,12 @@
 const { test, describe, beforeEach, expect } = require('@playwright/test');
-const { initializeDB, loginWith, createBlog } = require('./helper');
+const { 
+  initializeDB,
+  login,
+  logout,
+  createBlog,
+  createBlogWithOtherUser,
+  like
+} = require('./helper');
 
 describe('Blog App', () => {
   beforeEach(async ({ page, request }) => {
@@ -16,18 +23,18 @@ describe('Blog App', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await loginWith(page, 'FooBar', 'salainen123');
+      await login(page);
       await expect(page.getByText('John Doe logged in')).toBeVisible();
     });
   
     test('fails with wrong credentials', async ({ page }) => {
-      await loginWith(page, 'FooBar', 'sekret');
+      await login(page, 'FooBar', 'sekret', true);
       await expect(page.getByText('wrong username or password')).toBeVisible();
     });
 
     describe('When logged in', () => {
       beforeEach(async ({ page }) => {
-        await loginWith(page, 'FooBar', 'salainen123');
+        await login(page);
       });
 
       test('user can logout', async ({ page }) => {
@@ -50,6 +57,63 @@ describe('Blog App', () => {
 
         await expect(page.getByText(`\`${title}\` by ${author} added!`)).toBeVisible();
         await expect(page.getByText(`${title} by ${author}`)).toBeVisible();
+      });
+
+      describe('When some blogs saved', () => {
+        beforeEach(async ({ page }) => {
+          await createBlog(
+            page,
+            'The Odyssey',
+            'Cristofer Nolan',
+            'https://example.com'
+          );
+
+          await createBlog(
+            page,
+            'Avengers Doomsday',
+            'Marvel',
+            'www.coming.com'
+          );
+
+          await createBlogWithOtherUser(
+            page,
+            'Friends',
+            'Netflix',
+            'www.netflix.com'
+          );
+        });
+
+        test('more information about blog can be viewed', async ({ page }) => {  
+          const blogElement = await page
+            .locator('.blog')
+            .filter({ hasText: 'The Odyssey by Cristofer Nolan' });
+
+          await expect(blogElement.getByText('likes: 0')).not.toBeVisible();
+          await expect(blogElement.getByText('https://example.com')).not.toBeVisible();
+          await expect(blogElement.getByRole('button', { name: 'like' })).not.toBeVisible();
+          await expect(blogElement.getByText('John Doe')).not.toBeVisible();
+          
+          await blogElement.getByRole('button', { name: 'view' }).click();
+          await expect(blogElement.getByText('https://example.com')).toBeVisible();
+          await expect(blogElement.getByText('likes: 0')).toBeVisible();
+          await expect(blogElement.getByRole('button', { name: 'like' })).toBeVisible();
+          await expect(blogElement.getByText('John Doe')).toBeVisible();
+        });
+
+        test('blog can be liked', async ({ page }) => {
+          const blogElement = await page
+            .locator('.blog')
+            .filter({ hasText: 'Friends by Netflix' });
+          
+          await blogElement.getByRole('button', { name: 'view' }).click();
+          await expect(blogElement.getByText('likes: 0')).toBeVisible();
+
+          await like(blogElement);
+          await expect(blogElement.getByText('likes: 1')).toBeVisible();
+
+          await like(blogElement);
+          await expect(blogElement.getByText('likes: 2')).toBeVisible();
+        });
       });
     });
   });
--- bloglist/e2e-tests/tests/helper.js
+++ bloglist/e2e-tests/tests/helper.js
@@ -1,32 +1,74 @@
+const { expect } = require('@playwright/test');
+
+const users = [
+  {
+    name: 'John Doe',
+    username: 'FooBar',
+    password: 'salainen123',
+  },
+  {
+    name: 'Samu Hytönen',
+    username: 'hytosama',
+    password: 'sekret123',
+  },
+];
+
 const initializeDB = async (req) => {
-  const response1 = await req.post('/api/testing/reset');
-  const response2 = await req.post('/api/users', {
-    data: {
-      name: 'John Doe',
-      username: 'FooBar',
-      password: 'salainen123',
-    }
+  await req.post('/api/testing/reset');
+  await req.post('/api/users', {
+    data: users[0]
+  });
+  await req.post('/api/users', {
+    data: users[1]
   });
 };
 
-const loginWith = async (page, username, password) => {
+const login = async (
+  page,
+  username = users[0].username,
+  password = users[0].password,
+) => {
   await page.getByLabel('username').fill(username);
   await page.getByLabel('password').fill(password);
   await page.getByRole('button', { name: 'login' }).click();
 };
 
+const logout = async (page) => {
+  await page.getByRole('button', { name: 'logout' }).click();
+};
+
 const createBlog = async (page, title, author, url) => {
   await page.getByRole('button', { name: 'new blog' }).click();
+  
   await page.getByLabel('title').fill(title);
   await page.getByLabel('author').fill(author);
   await page.getByLabel('url').fill(url);
+  
   await page.getByRole('button', { name: 'create' }).click();
+  await page.getByText(`${title} by ${author}`).waitFor();
+  
+  await page.getByRole('button', { name: 'cancel' }).click();
+};
 
-  // await page.getByText(`${title} by ${author}`).waitFor();
+const createBlogWithOtherUser = async (page, title, author, url) => {
+  await logout(page);
+  await login(page, users[1].username, users[1].password);
+
+  await createBlog(page, title, author, url);
+  await logout(page);
+
+  await login(page);
+};
+
+const like = async (blogElement) => {
+  await blogElement.getByRole('button', { name: 'like' }).click();
 };
 
 export {
   initializeDB,
-  loginWith,
+  login,
+  logout,
   createBlog,
+  createBlogWithOtherUser,
+  like,
 }
